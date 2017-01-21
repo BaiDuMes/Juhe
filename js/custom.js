ab48af8a9ee0fb70a0e0490cda24f9b2315ec47a
BaiDuMes <BaiDuMes@163.com>
2017-01-21 15:43:53 +0800

    聚合支付

---
+++ js/custom.js
@@ -0,0 +1,130 @@
+
+
+/* ----------------------------- 
+NiceScroll
+----------------------------- */	
+/*$(document).ready(function() { 
+	'use strict';
+	$("html").niceScroll({
+		cursorcolor: '#E74C3C',
+		cursoropacitymin: '1',
+		cursorborder: '0px',
+		cursorborderradius: '0px',
+		cursorwidth: '5px',
+		cursorminheight: 60,
+		horizrailenabled: false,
+		zindex: 1090
+	});
+});
+*/
+
+/* ----------------------------- 
+Scroll into viewPort Animation
+----------------------------- */
+$(document).ready(function() {	
+	// 'use strict';
+	$('.animated').appear(function() {
+		var element = $(this),
+		animation = element.data('animation'),
+		animationDelay = element.data('animation-delay');
+		if ( animationDelay ) {
+
+			setTimeout(function(){
+				element.addClass( animation + " visible");
+			}, animationDelay);
+
+		} else {
+			element.addClass( animation + " visible");
+		}
+	});
+});
+
+
+/* -----------------------------
+Card Style Script
+----------------------------- */					
+$(document).ready(function() {
+	//'use strict';
+	var $el 			= $( '#card-ul' ),
+	sectionFeature  = $('#section-feature'),
+	baraja 			= $el.baraja();
+	
+	if ( $(window).width() > 480) {
+		sectionFeature.appear(function(){
+			baraja.fan({
+				speed : 1500,
+				easing : 'ease-out',
+				range : 100,
+				direction : 'right',
+				origin : { x : 50, y : 200 },
+				center : true
+			});
+		});
+		$('#feature-expand').click(function() {
+			baraja.fan({
+				speed : 500,
+				easing : 'ease-out',
+				range : 100,
+				direction : 'right',
+				origin : { x : 50, y : 200 },
+				center : true
+			});
+		}); 
+	} else {
+		sectionFeature.appear(function(){
+			baraja.fan({
+				speed : 1500,
+				easing : 'ease-out',
+				range : 80,
+				direction : 'left',
+				origin : { x : 200, y : 50 },
+				center : true
+			});
+		});
+		$('#feature-expand').click(function() {
+			baraja.fan({
+				speed : 500,
+				easing : 'ease-out',
+				range : 80,
+				direction : 'left',
+				origin : { x : 200, y : 50 },
+				center : true
+			});
+		});
+	}
+	
+	// Feature navigation
+	$('#feature-prev').on( 'click', function( event ) {
+		baraja.previous();
+	});
+
+	$('#feature-next').on( 'click', function( event ) {
+		baraja.next();
+	});
+	
+	// close Features
+	$('#feature-close').on( 'click', function( event ) {
+		baraja.close();
+	});	
+});
+
+
+/* ----------------------------- 
+IE 9 Placeholder fix
+----------------------------- */
+$('[placeholder]').focus(function() {
+	var input = $(this);
+	if (input.val() == input.attr('placeholder')) {
+		input.val('');
+		input.removeClass('placeholder');
+	}
+}).blur(function() {
+	var input = $(this);
+	if (input.val() == '' || input.val() == input.attr('placeholder')) {
+		input.addClass('placeholder');
+		input.val(input.attr('placeholder'));
+	}
+}).blur();
+
+
+
